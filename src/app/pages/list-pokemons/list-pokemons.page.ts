a6e1510dd8f6504a0f091c4cc07eb3fb5e6db89d
Cristan Medina <crishighlights@gmail.com>
2025-10-28 10:09:31 -0700

    Parte 9

--- src/app/pages/list-pokemons/list-pokemons.page.ts
+++ src/app/pages/list-pokemons/list-pokemons.page.ts
@@ -1,27 +1,38 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { PokemonService } from '../../services/pokemon';
+import { PokemonInterface } from 'src/app/interfaces/pokemon';
 
 @Component({
   selector: 'app-list-pokemons',
   templateUrl: './list-pokemons.page.html',
   styleUrls: ['./list-pokemons.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, JsonPipe]
 })
 export class ListPokemonsPage {
 
-  constructor() { }
+
   private pokemonService: PokemonService = inject(PokemonService);
+
+  pokemons: PokemonInterface[] = [];
+
+  constructor() { }
 
   ionViewWillEnter(){
     this.getMorePokemons();
   }
 
   getMorePokemons(){
-    this.pokemonService.getPokemons();
+    const promisePokemons = this.pokemonService.getPokemons();
+
+    if(promisePokemons){
+        promisePokemons.then(( pokemons: PokemonInterface[] ) => {
+            this.pokemons = this.pokemons.concat(pokemons);
+        })
+    }
   }
 
 }
